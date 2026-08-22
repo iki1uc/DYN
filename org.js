03cf3e50fe058d2d6522fa910f5e2bbe4da4ee5b
iki1uc <entertrainer@web.de>
2026-08-22 08:51:11 +0200

    Create org.js

---
+++ org.js
@@ -0,0 +1,25 @@
+export function dyn(v) {
+    return v;
+}
+import { impulse } from "./impulse.js";
+import { schub } from "./schub.js";
+import { feuer } from "./feuer.js";
+import { VOLTAGE } from "./VOLTAGE.js";
+import { RUM } from "./RUM.js";
+import { RUMumRUM } from "./RUMumRUM.js";
+import { nc } from "./nc.js";
+import { role } from "./role.js";
+
+export function org(v) {
+
+    return {
+        impulse: impulse(v),
+        schub: schub(v),
+        feuer: feuer(v),
+        voltage: VOLTAGE(v),
+        rum: RUM(v),
+        rumumrum: RUMumRUM(v),
+        neutral: nc(v),
+        role: role(v)
+    };
+}
