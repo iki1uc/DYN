9ef98503a7091843462ad44d1a5f429c0e57770b
iki1uc <entertrainer@web.de>
2026-08-22 08:45:07 +0200

    Update Dynamik.js

--- Dynamik.js
+++ Dynamik.js
@@ -1,1 +1,17 @@
+// Dynamik.js
+export const Dynamik = {
+
+    value: 0,
+
+    update(v) {
+        this.value = v;
+
+        return {
+            impulse: v * 1.0,
+            schub: v * 1.2,
+            feuer: v * 1.4,
+            voltage: v * 1.6
+        };
+    }
+};
 
