906863b417742be2c8a725c6343a20f760b8dd26
iki1uc <entertrainer@web.de>
2026-08-22 08:51:36 +0200

    Create reORG.js

---
+++ reORG.js
@@ -0,0 +1,14 @@
+export function reORG(obj) {
+
+    return {
+        clean: true,
+        impulse: obj.impulse,
+        schub: obj.schub,
+        feuer: obj.feuer,
+        voltage: obj.voltage,
+        rum: obj.rum,
+        rumumrum: obj.rumumrum,
+        neutral: obj.neutral,
+        role: obj.role
+    };
+}
