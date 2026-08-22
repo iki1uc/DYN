c4e5c13ebc699026ebf3cf354b65a59a6750559a
iki1uc <entertrainer@web.de>
2026-08-22 09:02:51 +0200

    Update dyn.js

--- dyn.js
+++ dyn.js
@@ -1,11 +1,28 @@
-// dyn.js
+// dyn.js – 3D + amplify/reduce (depth‑konform)
+
 export const dyn = {
 
     amplify(v, factor = 1) {
-        return v * factor;
+        return {
+            x: v * factor,
+            y: v * factor,
+            z: v * factor
+        };
     },
 
     reduce(v, factor = 1) {
-        return v / factor;
+        return {
+            x: v / factor,
+            y: v / factor,
+            z: v / factor
+        };
+    },
+
+    base(v) {
+        return {
+            x: v,
+            y: v,
+            z: v
+        };
     }
 };
